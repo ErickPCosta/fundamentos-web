16afa8b7059dce1e4b9f032d4dd43b3ecf7fb83b
unknown <erick.costa362@gmail.com>
2022-01-17 06:08:58 -0300

    Implementando JS

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+/*
+Case Sensitive = reconhece letras maiusculas e minusculas
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementsByName()
+por Classe: getElementsByClassName()
+por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById("nome")
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+let txtNome = document.querySelector("#txtNome")
+
+        if(nome.value.length < 3) {
+            txtNome.innerHTML = "Nome Inválido"
+            txtNome.style.color = "red"
+        } else {
+            txtNome.innerHTML = "Nome Válido"
+            txtNome.style.color = "green"
+        }
+
+    }
+
+function validaEmail() {
+    let txtmail = document.querySelector("#txtEmail")
+
+    if (email.value.indexOf("@") == -1 ) {
+        txtEmail.innerHTML = "E-mail Inváldio"
+        txtEmail.style.color = "red"
+    }
+ else {
+    txtNome.innerHTML = "E-mail Válido"
+    txtNome.style.color = "green"
+}
+
+}
